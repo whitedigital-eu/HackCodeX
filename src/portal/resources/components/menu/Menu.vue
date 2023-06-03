a70e5bd7c7895b2c5dc09f054a1b0529982c5078
kulers <kulershk@gmail.com>
2023-06-03 16:49:48 +0300

    Frontend base

---
+++ src/portal/resources/components/menu/Menu.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type {MenuInterface} from "../../interfaces/menu/MenuInterface";
+
+const topMenu = ref<MenuInterface[]>([
+  {
+    label: 'Par mums',
+    location: 'about',
+  },
+  {
+    label: 'Aptauja',
+    location: 'survey',
+  },
+  {
+    label: 'Atrodi savu nākotni',
+    location: 'future',
+  }
+])
+</script>
+
+<template>
+  <div class="top-bar">
+    <div class="menu">
+      <router-link v-for="item in topMenu" active-class="active" :to="item.location">{{ item.label}}</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.top-bar {
+  .menu {
+    background: #232325;
+    display: flex;
+    justify-content: center;
+    flex-wrap: nowrap;
+    gap: 10px;
+
+    a {
+      padding: 10px;
+      margin: 18px 0;
+      color: #FFFFFF;
+      display: inline-block;
+      border-radius: 20px;
+      font-size: 16px;
+      font-weight: 600;
+
+      &.active {
+        background: #F32E81;
+      }
+    }
+  }
+}
+</style>
