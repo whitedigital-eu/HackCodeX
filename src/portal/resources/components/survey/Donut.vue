0b97fb3a177ceb0671e83d0d0ae466d97d602872
kulers <kulershk@gmail.com>
2023-06-05 08:48:20 +0300

    front

--- src/portal/resources/components/survey/Donut.vue
+++ src/portal/resources/components/survey/Donut.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <svg width="100%" height="100%" viewBox="0 0 40 40" class="donut">
+  <svg height="100%" viewBox="0 0 40 40" class="donut">
     <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="transparent"></circle>
     <circle class="donut-segment donut-segment" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="8" :stroke-dasharray="`${percent} ${ 100 - percent}`" stroke-dashoffset="25"></circle>
   </svg>
